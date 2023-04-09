9b582833da07d40c5986f878e4fe71e5f0f7aacb
medilies <medilies@gmail.com>
2023-04-10 00:12:46 +0100

    broadcastIp

--- src/libs/Ipv4/SubNet.ts
+++ src/libs/Ipv4/SubNet.ts
@@ -4,6 +4,7 @@ export default class SubNet {
     networkIp: Ip
     mask: Ip
     firstHostIp: Ip
+    broadcastIp: Ip
 
     constructor(anyIp: Ip | string, mask: Ip | string) {
         anyIp = anyIp instanceof Ip ? anyIp : new Ip(anyIp)
@@ -11,6 +12,7 @@ export default class SubNet {
         this.mask = mask instanceof Ip ? mask : new Ip(mask)
         this.networkIp = this._resolveIp(anyIp)
         this.firstHostIp = this._resolveFirstHostIp(this.networkIp)
+        this.broadcastIp = this._resolveBroadcastIp(this.networkIp)
     }
 
     private _resolveIp(anyIp: Ip): Ip {
@@ -36,11 +38,48 @@ export default class SubNet {
         return new Ip(firstHostIp.join('.'))
     }
 
+    private _resolveBroadcastIp(networkIp: Ip): Ip {
+        const broadcastIpBin = networkIp
+            .getBinValue()
+            .split('')
+            .map(
+                (bit, index) =>
+                    parseInt(bit) | parseInt(this._getWildcardMask().getBinValue()[index])
+            )
+            .join('')
+
+        const broadcastIpDecimal = broadcastIpBin
+            .match(/.{1,8}/g)!
+            .map((octet) => parseInt(octet, 2))
+            .join('.')
+
+        return new Ip(broadcastIpDecimal)
+    }
+
+    private _getWildcardMask(): Ip {
+        const wildcardMaskBin = this.mask
+            .getBinValue()
+            .split('')
+            .map((bit) => parseInt(bit) ^ 1)
+            .join('')
+
+        const wildcardMaskDecimal = wildcardMaskBin
+            .match(/.{1,8}/g)!
+            .map((octet) => parseInt(octet, 2))
+            .join('.')
+
+        return new Ip(wildcardMaskDecimal)
+    }
+
     getNetworkIp(): Ip {
         return this.networkIp
     }
 
     getFirstHostIp(): Ip {
         return this.firstHostIp
+    }
+
+    getBroadcastIp(): Ip {
+        return this.broadcastIp
     }
 }
--- src/libs/__tests__/Subnet.test.ts
+++ src/libs/__tests__/Subnet.test.ts
@@ -9,35 +9,40 @@ describe('SubNet', () => {
             inputIp: '192.168.1.100',
             mask: '255.255.255.0',
             networkIp: '192.168.1.0',
-            firstHostIp: '192.168.1.1'
+            firstHostIp: '192.168.1.1',
+            broadcastIp: '192.168.1.255'
         },
         {
             inputIp: '10.0.0.1',
             mask: '255.255.0.0',
             networkIp: '10.0.0.0',
-            firstHostIp: '10.0.0.1'
+            firstHostIp: '10.0.0.1',
+            broadcastIp: '10.0.255.255'
         },
         {
             inputIp: '172.16.0.100',
             mask: '255.255.255.128',
             networkIp: '172.16.0.0',
-            firstHostIp: '172.16.0.1'
+            firstHostIp: '172.16.0.1',
+            broadcastIp: '172.16.0.127'
         },
         {
             inputIp: '192.168.0.1',
             mask: '255.255.255.252',
             networkIp: '192.168.0.0',
-            firstHostIp: '192.168.0.1'
+            firstHostIp: '192.168.0.1',
+            broadcastIp: '192.168.0.3'
         }
     ]
 
     it.each(testData)(
         'should correctly calculate the network IP address for %s\n',
-        ({ inputIp, mask, networkIp, firstHostIp }) => {
+        ({ inputIp, mask, networkIp, firstHostIp, broadcastIp }) => {
             const subnet = new SubNet(inputIp, mask)
 
             expect(subnet.getNetworkIp()).toEqual(new Ip(networkIp))
             expect(subnet.getFirstHostIp()).toEqual(new Ip(firstHostIp))
+            expect(subnet.getBroadcastIp()).toEqual(new Ip(broadcastIp))
         }
     )
 })
