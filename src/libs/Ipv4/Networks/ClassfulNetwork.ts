e815f6263902d3723ad9cc0ea516e6525b1c579d
medilies <medilies@gmail.com>
2023-04-16 18:59:37 +0100

    ClassfulNetwork

---
+++ src/libs/Ipv4/Networks/ClassfulNetwork.ts
@@ -0,0 +1,37 @@
+import Mask from '@/libs/Ipv4/Addresses/Mask'
+import type IpAddress from '@/libs/Ipv4/Addresses/IpAddress'
+import Network from '@/libs/Ipv4/Networks/Network'
+import DecimalFormat from '@/libs/Ipv4/Formats/DecimalFormat'
+
+export default class ClassfulNetwork extends Network {
+    readonly class: string
+
+    constructor(anyIp: IpAddress) {
+        super(anyIp, ClassfulNetwork._resolveMaskFromClass(ClassfulNetwork._resolveClass(anyIp)))
+
+        this.class = ClassfulNetwork._resolveClass(anyIp)
+    }
+
+    public static _resolveClass(anyIp: IpAddress): string {
+        const firstOctet = anyIp.decimalValue.octets[0]
+
+        if (firstOctet >= 0 && firstOctet <= 127) return 'A'
+        if (firstOctet >= 128 && firstOctet <= 191) return 'B'
+        if (firstOctet >= 192 && firstOctet <= 223) return 'C'
+        if (firstOctet >= 224 && firstOctet <= 239) return 'D'
+        if (firstOctet >= 240 && firstOctet <= 255) return 'E'
+        throw new Error(`Invalid IP address: ${anyIp.decimalValue.value}`)
+    }
+
+    public static _resolveMaskFromClass(classType: string): Mask {
+        const maskMap = new Map<string, string>([
+            ['A', '255.0.0.0'],
+            ['B', '255.255.0.0'],
+            ['C', '255.255.255.0'],
+            ['D', '240.0.0.0'], // ! N/A (these are multicast addresses)
+            ['E', '248.0.0.0'] // ! N/A (these are reserved for future use)
+        ])
+
+        return new Mask(new DecimalFormat(maskMap.get(classType)!))
+    }
+}
---
+++ src/libs/Ipv4/Networks/__tests__/ClassfulNetwork.test.ts
@@ -0,0 +1,73 @@
+import { describe, expect, it } from 'vitest'
+import ClassfulNetwork from '@/libs/Ipv4/Networks/ClassfulNetwork'
+import DecimalFormat from '@/libs/Ipv4/Formats/DecimalFormat'
+import IpAddress from '@/libs/Ipv4/Addresses/IpAddress'
+
+describe('Network', () => {
+    describe('subnet construction+getters', () => {
+        it.each([
+            {
+                inputIp: '192.168.1.100',
+                expectedClass: 'C',
+                mask: '255.255.255.0',
+                prefix: 24,
+                size: 254,
+                wildCardMask: '0.0.0.255',
+                networkIp: '192.168.1.0',
+                firstHostIp: '192.168.1.1',
+                lastHostIp: '192.168.1.254',
+                broadcastIp: '192.168.1.255'
+            },
+            {
+                inputIp: '10.0.0.1',
+                expectedClass: 'A',
+                mask: '255.0.0.0',
+                prefix: 8,
+                size: 16777214,
+                wildCardMask: '0.255.255.255',
+                networkIp: '10.0.0.0',
+                firstHostIp: '10.0.0.1',
+                lastHostIp: '10.255.255.254',
+                broadcastIp: '10.255.255.255'
+            },
+            {
+                inputIp: '172.16.0.100',
+                expectedClass: 'B',
+                mask: '255.255.0.0',
+                prefix: 16,
+                size: 65534,
+                wildCardMask: '0.0.255.255',
+                networkIp: '172.16.0.0',
+                firstHostIp: '172.16.0.1',
+                lastHostIp: '172.16.255.254',
+                broadcastIp: '172.16.255.255'
+            }
+        ])(
+            'should correctly calculate the network IP address for %s',
+            ({
+                inputIp,
+                expectedClass,
+                mask,
+                prefix,
+                size,
+                wildCardMask,
+                networkIp,
+                firstHostIp,
+                lastHostIp,
+                broadcastIp
+            }) => {
+                const network = new ClassfulNetwork(new IpAddress(new DecimalFormat(inputIp)))
+
+                expect(network.class).toEqual(expectedClass)
+                expect(network.mask.decimalValue.value).toEqual(mask)
+                expect(network.prefix.value).toEqual(prefix)
+                expect(network.size).toEqual(size)
+                expect(network.wildcardMask.decimalValue.value).toEqual(wildCardMask)
+                expect(network.networkAddress.decimalValue.value).toEqual(networkIp)
+                expect(network.firstHostAddress.decimalValue.value).toEqual(firstHostIp)
+                expect(network.broadcastAddress.decimalValue.value).toEqual(broadcastIp)
+                expect(network.lastHostAddress.decimalValue.value).toEqual(lastHostIp)
+            }
+        )
+    })
+})
